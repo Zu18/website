ade397a0a57a24aec2a5651dc99e00cf4aa632c5
Zukhra <zuhra.galieva.2014@gmail.com>
2023-06-29 13:56:13 +0100

    Add Memory game project, style background-images

--- my-script.js
+++ my-script.js
@@ -1,5 +1,5 @@
 window.addEventListener("scroll", function scrollNavbar() {
-    var nav = document.querySelector("#myNavbar");
+    let nav = document.querySelector("#myNavbar");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         nav.classList.add("topNavbar-scrolled");
     } else {
@@ -7,3 +7,16 @@ window.addEventListener("scroll", function scrollNavbar() {
     }
 });
 
+let coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
